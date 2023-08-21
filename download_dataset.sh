a22adf788b6c671eee6fd30273233a205caca174
Anna Petrasova <kratochanna@gmail.com>
2023-08-21 15:13:07 +0000

    simplify example, add bash/python tool syntax, make wget silent

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,4 +1,4 @@
-wget https://zenodo.org/record/8206464/files/FUTURES_Triangle.zip
+wget -q https://zenodo.org/record/8206464/files/FUTURES_Triangle.zip
 unzip FUTURES_Triangle.zip
 mv FUTURES_Triangle opengeohub_2023
 rm FUTURES_Triangle.zip
